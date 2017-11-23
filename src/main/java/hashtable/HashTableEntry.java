09bd5e57d40fa80e8e8d16833e86d19c688cf950
Anchal Khandelwal <ankhande@iu.edu>
2017-11-22 19:58:49 -0600

    hashtable design structure hashtable Entry class added

---
+++ src/main/java/hashtable/HashTableEntry.java
@@ -0,0 +1,27 @@
+package hashtable;
+
+public class HashTableEntry {
+	private int key;
+	private int value;
+	
+	HashTableEntry(int key, int value){
+		this.key = key;
+		this.value = value;
+	}
+
+	public int getKey() {
+		return key;
+	}
+	
+	public int getValue() {
+		return value;
+	}
+
+//	public void setValue(int value) {
+//		this.value = value;
+//	}
+//	
+//	public void setKey(int key) {
+//		this.key = key;
+//	}	
+}
